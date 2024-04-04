d15dda181947ea9251d86c422729b84b255632e5
KsanaSal <oasalivon@gmail.com>
2024-04-04 23:03:22 +0300

    setting project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "sky-medium": "#4CB7FF",
+                "sky-dark": "#067DCE",
+                "sky-black": "#032135",
+                "green-light": "#EFFCEA",
+                "green-medium": "#58FC1A",
+                "green-dark": "#113703",
+            },
+        },
+        fontFamily: {
+            sans: ["Georgia", "sans-serif"],
+            serif: ["Merriweather", "serif"],
+            display: ["Oswald"],
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
